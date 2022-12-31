a02f50f2f2ff2f188b8a6687f67aa27604470d33
bribeyerl <briabeyerl@gmail.com>
2022-12-30 20:47:54 -0500

    Add pseudocode and variables

--- main.js
+++ main.js
@@ -1,8 +1,19 @@
 const choices = ['rock', 'paper', 'scissors'];
+let computerSelection;
+let playerSelection;
 
 function getComputerChoice() {
     const randomLogic = Math.floor(Math.random() * choices.length)
-    let computerResponse = choices[randomLogic]
+    let computerSelection = choices[randomLogic]
     console.log(computerResponse)
 }
 
+// function getPlayerSelection() {
+//     ask for selection in a dialog box using window.prompt()
+//     store response in playerSelection var
+//     change toLower 
+// }
+
+// function playRound(playerSelection, computerSelection) {
+
+// }
